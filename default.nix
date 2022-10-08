6aaaddcc7c4c89fd72a897f7697191d7911ec161
GermanBread <nd01@online.de>
2022-10-08 09:58:20 +0200

    fix license

--- default.nix
+++ default.nix
@@ -21,7 +21,7 @@ buildDotnetModule rec {
   meta = with lib; {
     homepage = "https://github.com/ProjectCuteAndFunny/CunnyCLI";
     description = "A tool that uses the CunnyAPI to download images";
-    license = licenses.mit;
+    license = licenses.gpl3;
     platforms = [ "x86_64-linux" ];
   };
 }
