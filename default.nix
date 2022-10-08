86e1b177c14711c4ba6f7423a253eccd513bf835
GermanBread <nd01@online.de>
2022-10-08 10:26:11 +0200

    fix syntax error

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ buildDotnetModule rec {
   version = "1";
   src = ./.;
   nugetDeps = ./deps.nix;
-s  dotnet-sdk = dotnet-sdk_6;
+  dotnet-sdk = dotnet-sdk_6;
   dotnet-runtime = dotnet-runtime_6;
   meta = with lib; {
     homepage = "https://github.com/ProjectCuteAndFunny/CunnyCLI";
